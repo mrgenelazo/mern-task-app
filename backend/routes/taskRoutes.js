0fa0432b42f05566eef7d46cfc54893f959af9cb
Dennis Genelazo <DGenelazo@boylesports.com>
2023-04-26 17:30:35 +0800

    create task model schema

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const Task = require("../model/TaskModel");
+
+const router = express.Router()
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,39 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const Task = require("./model/TaskModel");
 
 const app = express();
 
-//Routes
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
+
+//Routes
 app.get("/", (req, res) => {
     res.send("Home Page");
 })
 
+//Create Task
+app.post("/api/tasks", async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(500).json({msg: error.message})      
+    }
+});
+
+//Get/Read Data
+app.get("/api/tasks", async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks)
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+});
 
 
 const PORT = process.env.PORT || 5000
