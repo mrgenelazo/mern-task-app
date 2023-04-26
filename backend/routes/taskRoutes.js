e2d55d69a6de4027712f7382773f057852c65ade
Dennis Genelazo <DGenelazo@boylesports.com>
2023-04-26 17:38:34 +0800

    create routes

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,4 +1,27 @@
 const express = require("express");
 const Task = require("../model/TaskModel");
 
-const router = express.Router()
+const router = express.Router();
+
+
+//Create Task
+router.post("/api/tasks", async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(500).json({msg: error.message})      
+    }
+});
+
+//Get/Read Tasks
+router.get("/api/tasks", async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks)
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+});
+
+module.exports = router; 
--- backend/server.js
+++ backend/server.js
@@ -2,39 +2,19 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const Task = require("./model/TaskModel");
+const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
+app.use(taskRoutes);
 
 //Routes
 app.get("/", (req, res) => {
     res.send("Home Page");
 })
-
-//Create Task
-app.post("/api/tasks", async (req, res) => {
-    try {
-        const task = await Task.create(req.body)
-        res.status(200).json(task)
-    } catch (error) {
-        res.status(500).json({msg: error.message})      
-    }
-});
-
-//Get/Read Data
-app.get("/api/tasks", async (req, res) => {
-    try {
-        const tasks = await Task.find();
-        res.status(200).json(tasks)
-    } catch (error) {
-        res.status(500).json({msg: error.message});
-    }
-});
-
 
 const PORT = process.env.PORT || 5000
 
